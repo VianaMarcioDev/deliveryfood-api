44b1726897ac20afc59dec8c0040fc2f010a79fd
Marcio de Freitas Viana <marcioviana.developer@gmail.com>
2025-07-26 17:42:52 -0300

    feat: adicionar injecao de dependencia notificador

--- src/main/java/br/com/marcioviana/deliveryfood/di/service/AtivacaoClienteService.java
+++ src/main/java/br/com/marcioviana/deliveryfood/di/service/AtivacaoClienteService.java
@@ -1,13 +1,18 @@
 package br.com.marcioviana.deliveryfood.di.service;
 
 import br.com.marcioviana.deliveryfood.di.modelo.Cliente;
+import br.com.marcioviana.deliveryfood.di.notificacao.Notificador;
 import br.com.marcioviana.deliveryfood.di.notificacao.NotificadorEmail;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
-    private NotificadorEmail notificador;
+    private Notificador notificador;
+
+    public AtivacaoClienteService(Notificador notificador) {
+        this.notificador = notificador;
+    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
