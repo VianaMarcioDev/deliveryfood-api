cbe6dae56c5b5eda0110b12cb77298a5c3276d8a
Marcio de Freitas Viana <marcioviana.developer@gmail.com>
2025-07-26 17:43:46 -0300

    feat: adicionar injecao de dependencia ativacaoClienteService

--- src/main/java/br/com/marcioviana/deliveryfood/MeuPrimeiroController.java
+++ src/main/java/br/com/marcioviana/deliveryfood/MeuPrimeiroController.java
@@ -1,5 +1,7 @@
 package br.com.marcioviana.deliveryfood;
 
+import br.com.marcioviana.deliveryfood.di.modelo.Cliente;
+import br.com.marcioviana.deliveryfood.di.service.AtivacaoClienteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +9,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MeuPrimeiroController {
 
+    private AtivacaoClienteService atividacaoClienteService;
+
+    public MeuPrimeiroController(AtivacaoClienteService atividacaoClienteService) {
+        this.atividacaoClienteService = atividacaoClienteService;
+    }
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
+        Cliente aldilene = new Cliente("Aldilene", "lene@xyz.com", "11911111111");
+
+        atividacaoClienteService.ativar(aldilene);
+
         return "hello, Márcio";
     }
 }
